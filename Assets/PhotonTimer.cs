390fc74d49a814a6a3cfa6639eb5d576f71f6bf9
Stephen-PC\Stephen <nostalgicbear@hotmail.com>
2020-06-09 21:10:55 +0100

    Start of basic timer

---
+++ Assets/PhotonTimer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+
+public class PhotonTimer : MonoBehaviourPunCallbacks
+{
+    public TextMeshProUGUI roundTimer;
+    public double totalRoundTime = 50;
+    public double currentRoundTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.LogError("People in room is " + PhotonNetwork.CurrentRoom.PlayerCount);
+        if(!PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+
+        }
+
+        currentRoundTime = totalRoundTime;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        DecreaseTime();
+        roundTimer.text = currentRoundTime.ToString();
+    }
+
+    private void DecreaseTime()
+    {
+        currentRoundTime -= Time.deltaTime;
+    }
+
+    [PunRPC]
+    public void SetCurrentTime(double currentTimeFromServer)
+    {
+        currentRoundTime = currentTimeFromServer;
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        Debug.LogError("RRR");
+    }
+
+}
