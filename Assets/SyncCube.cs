61f837887de7776dd83f5823efa8716a2ce09d69
Stephen-PC\Stephen <nostalgicbear@hotmail.com>
2020-06-10 16:07:39 +0100

    d

---
+++ Assets/SyncCube.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class SyncCube : MonoBehaviourPun, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+        }
+        else { 
+            transform.position = (Vector3)stream.ReceiveNext();
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
